2dd1d44f2d13534416a184a81bd22d5f6cdc481d
Kevin Prathama <kevin.prathama@vidio.com>
2022-03-23 15:29:41 +0700

    ADD snapshot testing

--- src/components/Hello/Hello.test.tsx
+++ src/components/Hello/Hello.test.tsx
@@ -3,11 +3,11 @@ import Hello from "./index";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-// test("snapshot", () => {
-//   const {asFragment} = render(<Hello name={"Khea"} />);
+test("snapshot", () => {
+  const {asFragment} = render(<Hello name={"Khea"} />);
 
-//   expect(asFragment()).toMatchSnapshot();
-// })
+  expect(asFragment()).toMatchSnapshot();
+})
 
 test("Hello Component", () => {
   render(<Hello name={"Khea"} />);
