68690eae5bda4fb966e465c2c1679c910f6f6a2f
Michael Kurniawan <kurniawan.michael24@gmail.com>
2022-04-06 14:10:25 +0700

    category fetch

--- src/components/category-app/Category.jsx
+++ src/components/category-app/Category.jsx
@@ -1,77 +1,52 @@
-import './Category.css'
-import React, {useState, useEffect } from 'react'
-import { deattribute, deserialise } from 'kitsu-core'
-import 'unfetch/polyfill'
-import { category } from './Category.stories'
-// import { response } from 'msw'
+import "./Category.css";
+import React, { useState, useEffect } from "react";
+import { deattribute, deserialise } from "kitsu-core";
+import "unfetch/polyfill";
+import { category } from "./Category.stories";
 
 function Category() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [data, setData] =  useState({})
-    const [isLoading, setIsLoading] = useState(true)
-    
-    //FETCH
-    useEffect(()=>{
-        fetch('./data/categoryData.json', {
-            headers: {
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(response => {
-            // console.error(response)
-            const parsedData = deserialise(response)
-            console.error(response)
-            setData(parsedData)
-            setIsLoading(false)
-        })
-        .catch(err => console.error(err))
-    }, [])    
+  //FETCH
+  useEffect(() => {
+    fetch("./data/categoryData.json")
+      .then((response) => response.json())
+      .then((response) => {
+        const parsedData = deserialise(response.data);
 
-    if(isLoading) return <p className='loading' data-testid="loading">LOADING</p>
+        // kenapa arraynya dari 0 kak?
+        // soalnya kan parsedData itu array isinya detail untuk 2 section
+        // kita mau ambil yg pertama dulu jadi pakai [0]        console.log("debug parsedData", pa)()
+        fetch(parsedData[0].links.self).then((response) => response.json());
 
-    return(
-        <>
-        <div className="Category">
-       
-          <ol>
-              {data.data.map(dataCategory => (
-                  <li>
-                      {dataCategory.attributes.title}
-                  </li>
-              ))}
-              
-          </ol>
+        // hmm kenapa error ya, harusnya dia fetch data/watchPage.json sih
+        // yang idx maksud ku
+        setData(parsedData);
+        setIsLoading(false);
+      })
+      .catch((err) => console.error(err));
+  });
+  // oke mengerti kak
+  if (isLoading) return <p className="loading">LOADING</p>;
+  // oke lanjut ke testnya ya buat
+  // michael coba format document lagi deh yg multiple request// soalnya kan tadi aku pake 2 sebelom2nya\
+  // ribet ya livesh  console.log("debug data", data);
+  // guys coba push dulu ini ke github deh, nanti aku screenshare dari gmeet pas ngerjainnya
+    // ribet ya liveshare ini, hahaha  console.log("debug data", data);
+  // oke k
+ return(
+           <>
+  <div  cssName="Category">
+       {d ata.map((dataCategory, idx) => (
+          <span>{dataCategory.attributes.title}</span>
+        ))}
+   <div>
+   ;  )
 
-          {data.data.map((dataCategory, idx) => {
-              return(
-                  <>
-                      <div className="category" key={idx}>
-                      {/* {dataCategory.relationships.category.data['id', 'type']} */}
-                      {dataCategory.relationships.category.data.id}
-                      {dataCategory.relationships.category.data.type}
-                      </div>
-
-                        <div className="content">
-                            {dataCategory.links.self}
-                        </div>
-                  </>
-              )
-          })}
-
-          {data.included.map((dataIncluded, idex) => {
-              return(
-                  <>
-                      <div className="included" key={idex}>
-                      {dataIncluded.links.self}
-                      </div>
-                  </>
-              )
-          })}
-        </div>
-        </>
-    )
 }
 
 export default Category;
+
+//sebelomnya aku pake ol - li kak mau dicoba gk kak?
+// oiya dah bs kak di storybooknya, kok tb2 cmn jadi 1 doang y datanya?
--- src/components/category-app/Category.test.jsx
+++ src/components/category-app/Category.test.jsx
@@ -1,56 +1,120 @@
 import React from "react";
-import { render, fireEvent, waitFor, screen, waitForElementToBeRemoved } from "@testing-library/react"
-import { rest } from 'msw'
+import {
+  render,
+  fireEvent,
+  waitFor,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { rest } from "msw";
 import categoryData from "./categoryData";
 import { setupServer } from "msw/node";
-import CategoryStories from './Category.stories'
-import Category from "./Category"
+import CategoryStories from "./Category.stories";
+import Category from "./Category";
 
-const server = setupServer(
-    // rest.get('./data/categoryData.json',
-    //     (req, res, ctx) => {
-    //         return res(ctx.json(categoryData))
-    //     })
-)
+const server = setupServer();
+// rest.get('./data/categoryData.json',
+//     (req, res, ctx) => {
+//         return res(ctx.json(categoryData))
+//     })
 
 beforeAll(() => server.listen());
 beforeEach(() => {
-    server.use(...CategoryStories.parameters.msw)
-})
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
-
+  server.use(...CategoryStories.parameters.msw);
+});
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 // criteria consumed API data with response array of sections informations
 // can display components from json based on variations [on going]
 // it will have 4 states:
-// - success with contents 
+// - success with contents
 // - success with no - content: print empty
 // - is - loading: shimmer(same as current m.vidio.com) [done]
 // - error: create an empty component(same as m.vidio.com)
 
-
 // test 1 -- fetching json
-test.only("loading expect to be render while fetching JSON file", async() => {
-    render(< Category />)
-    expect(screen.getByText("LOADING")).toBeVisible()
+test.only("loading expect to be render while fetching JSON file", async () => {
+  jest.spyOn(window, "fetch");
 
-    const heading = await screen.findByRole("heading")
-    screen.debug()
-    expect(heading).toBeVisible()
-    expect(screen.queryByText("LOADING")).not.toBeInTheDocument()
-})
+  render(<Category />);
 
-//test 2 
-test("can display components from json based on variations", async() => {
-    render(<Category/>)
+  expect(window.fetch).toBeCalledWith("./data/categoryData.json");
 
-    const category  = await screen.queryByRole('category-one')
-    expect()
-})
+  // btw ini salah satu aja ternyata guys
+  // soalnya loading hilang kan teks langsuyng muncul, ngga perlu await dua2nya
 
+  // await waitForElementToBeRemoved(screen.getByText("LOADING"));
+  await screen.findByText("Lanjut Nonton, Yuk!");
 
+  expect(window.fetch).toBeCalledWith("data/watchData.json");
+  // oke testnya merah krn kita belum implement ya
+  // iy kak
+  // berarti yang loading gk ush dipake kak?\
+  // oke siap kak
+  // iya ngga perlu, tadi aku cuma mau nunjukkin apa itu waitforelementtoberemoved aja
+  // oh jadi di react testing library itu ada 3 cara query
+  // getBy...
+  // queryBy..
 
+  // findBy.. hmm agak panjang jelasinnya, wait
 
+  // oh kalian familiar sama waitFor() ngga?
+  // eh getBytext tahu lah ya, dia buat query element berdasarkan text
+  // nah ada kasus di mana element itu ngga langsung muncul (misal nunggu query dulu atau loading)
+  // di sini kita pakai waitFor
 
+  // await waitFor(() => getByText("wasd")) bakal jalanin getByText terus2an selama 1 detik
+  // kalau ketemu, dia bakal balikin elementnya
+  // kalau ngga ketemu bakal error
+  // dan waitFor(() => getByText(..)) itu sama persis dengan findByText
 
+  // hadu pas tadi kutulis
+  // await screen.findByText(Nonton)
+  // itu sama dengan
+  // await waitFor(() => getByText(Nonton))
+  // bingung ngga? haha
+  // btw aku sampai jam 2 aja ya pairnya, nanti mau lanjut kerja
+  //okee kakkk
+  // Sedikit confuse tp mengerti gunanya, sipp pokknya kalau ada text yg belum langsung keluar, pakai find itu aja
+  // await waitFor(jest.fn());
+  //siap kak
+  //tp kalo bs ajarin yang multiple fetch dulu dong kak
+  // sipsip habis ini harusnya
+  //oke
+  screen.debug();
+  // expect(title).toBeInTheDocument;
+  // berarti buat test ke-2 ya kak? hmm test ke-2 buat title section ke-2 ya maksudmu? yg
+  //test("wait untill element shows lanjut nonton yuk")
+  // iy kak, oh boleh2, btw itu screen.findByText bisa juga ya jadi langsun findByText(Lanjut nonton yuk)
+
+  /**
+   * 1. render component
+   * 2. fetch data categories
+   * 3. deserialise
+   * 4. tampilin loading,
+   * 5. tampilin title section
+   * 6. fetch ulang untuk isi section
+   */
+  // expect(screen.getByText("LOADING")).toBeVisible()
+
+  // const heading = await screen.findByRole("heading")
+  // screen.debug()
+  // expect(heading).toBeVisible()
+  // expect(screen.queryByText("LOADING")).not.toBeInTheDocument()
+});
+
+//maksud aku yang ini
+test("wait until element shows lanjut nonton yuk", async () => {
+  render(<Category />);
+
+  const title = screen.findBy;
+});
+
+//test 2
+test("can display components from json based on variations", async () => {
+  render(<Category />);
+
+  const category = await screen.queryByRole("category-one");
+  expect();
+});
